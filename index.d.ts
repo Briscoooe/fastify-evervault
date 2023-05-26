9f9f2541d0c417f4306ac353acf168b4dc3e97d0
Briscoooe <brianbriscoe06@gmail.com>
2023-05-26 12:37:36 +0100

    version

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,23 @@
-import { FastifyPluginCallback } from 'fastify';
+import type { FastifyPluginAsync } from 'fastify';
 import Evervault from '@evervault/sdk';
-
-export interface FastifyEvervaultOptions {
-  /**
-   * Evervault API Key
-   *
-   * @docs https://docs.evervault.com/sdks/nodejs
-   */
-  apiKey: string;
-}
-
-
-export type FastifyEvervault = InstanceType<typeof Evervault>;
-
 declare module "fastify" {
   interface FastifyInstance {
-    evervault: FastifyEvervault;
+    evervault: InstanceType<typeof Evervault>;
   }
 }
-export const FastifyEvervaultPlugin: FastifyPluginCallback<FastifyEvervaultOptions>;
-export default FastifyEvervaultPlugin;
+type FastifyEvervault = FastifyPluginAsync<fastifyEvervault.FastifyEvervaultOptions>;
+declare namespace fastifyEvervault {
+  export interface FastifyEvervaultOptions {
+    /**
+     * Evervault API Key
+     *
+     * @docs https://docs.evervault.com/sdks/nodejs
+     */
+    apiKey: string;
+  }
+  export const FastifyEvervault: FastifyEvervault;
+  export { FastifyEvervault as default };
+}
+
+declare function fastifyEvervault(...params: Parameters<FastifyEvervault>): ReturnType<FastifyEvervault>
+export = fastifyEvervault
