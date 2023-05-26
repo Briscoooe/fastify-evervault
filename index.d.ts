e44ef89776d781f24c8602229fa519af7643aa65
Briscoooe <brianbriscoe06@gmail.com>
2023-05-26 12:03:07 +0100

    version

--- index.d.ts
+++ index.d.ts
@@ -11,7 +11,7 @@ export interface FastifyEvervaultOptions {
 }
 
 
-export type FastifyEvervault = typeof Evervault;
+export type FastifyEvervault = typeof Evervault.prototype;
 
 declare module "fastify" {
   interface FastifyInstance {
